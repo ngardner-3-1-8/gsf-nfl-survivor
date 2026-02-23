da78328158bdf704b6b5f25ad127028ce670ec8d
ngardner-3-1-8 <ngardner318@gmail.com>
2026-02-23 16:39:48 -0700

    Update BACKFILL-TEST.py

--- BACKFILL-TEST.py
+++ BACKFILL-TEST.py
@@ -2824,15 +2824,15 @@ def loop_through_simulations(date_str):
                 pass_prob = self.profiles['playcalling'].get((off, down, d_bucket, ctx))
                 if pass_prob is None: pass_prob = self.league_pass_rates.get((down, d_bucket, ctx), 0.55)
     
-                # WIND: severe penalty if over 20mph
-                if not is_dome and wind_speed > 20:
-                    pass_prob -= 0.15  # Heavy shift to run
-                elif not is_dome and wind_speed > 15:
-                    pass_prob -= 0.05
+####                # WIND: severe penalty if over 20mph
+####                if not is_dome and wind_speed > 20:
+####                    pass_prob -= 0.15  # Heavy shift to run
+####                elif not is_dome and wind_speed > 15:
+####                    pass_prob -= 0.05
                 
                 # RAIN/SNOW: slight shift to run to avoid drops/tips
-                if not is_dome and (is_rain or is_snow):
-                    pass_prob -= 0.05
+####                if not is_dome and (is_rain or is_snow):
+####                    pass_prob -= 0.05
                 
                 # Standard Adjustments
                 if phase == 'REG' and clock < 300:
