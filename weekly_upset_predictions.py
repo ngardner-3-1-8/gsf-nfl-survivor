87c37df9a4f85f78a96aeeffcfc875c91c79d023
ngardner-3-1-8 <ngardner318@gmail.com>
2026-03-25 09:29:02 -0600

    Update weekly_upset_predictions.py

--- weekly_upset_predictions.py
+++ weekly_upset_predictions.py
@@ -143,7 +143,7 @@ else:
 # --- CONFIGURATION ---
 TRAIN_FILE = f"nfl-pbp-data/nfl_games_with_schematic_data_2008_{target_year - 1}.csv"
 TEST_FILE = f"nfl-pbp-data/nfl_games_with_schematic_data_{target_year}_{target_year}.csv"
-OUTPUT_FILE = f"nfl-power-rankings/nfl_{target_year}_matchup_upset_predictions.csv"
+OUTPUT_FILE = f"nfl-power-ratings/nfl_{target_year}_matchup_upset_predictions.csv"
 
 # --- UPDATED CATEGORIES ---
 MATCHUP_CATEGORIES = [
