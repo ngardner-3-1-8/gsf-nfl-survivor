fe3165f44c171275ad53f46436c8bf8372cfe760
ngardner-3-1-8 <ngardner318@gmail.com>
2026-04-28 00:19:19 -0600

    Update base URL in Vite configuration

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/your-repo-name/',  // ← replace with your actual repo name
+  base: '/gsf-nfl-survivor/',  // ← replace with your actual repo name
 })
