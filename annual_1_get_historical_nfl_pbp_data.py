45878d81f094d9c01f9d12c31e6052439cb86b71
ngardner-3-1-8 <ngardner318@gmail.com>
2026-05-15 23:45:40 -0600

    Update annual_1_get_historical_nfl_pbp_data.py

--- annual_1_get_historical_nfl_pbp_data.py
+++ annual_1_get_historical_nfl_pbp_data.py
@@ -7,7 +7,7 @@ from scipy.stats import percentileofscore
 from datetime import datetime, date
 import warnings
 
-date = "09/01/2021"
+date = "09/01/2020"
 # 1. Get current date
 today = datetime.now()
 
