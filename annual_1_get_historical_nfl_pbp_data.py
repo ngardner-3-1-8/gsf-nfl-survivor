9c510268c6bc6a823f939406cc8fccdedc9973d9
ngardner-3-1-8 <ngardner318@gmail.com>
2026-05-15 23:51:26 -0600

    Update annual_1_get_historical_nfl_pbp_data.py

--- annual_1_get_historical_nfl_pbp_data.py
+++ annual_1_get_historical_nfl_pbp_data.py
@@ -7,11 +7,8 @@ from scipy.stats import percentileofscore
 from datetime import datetime, date
 import warnings
 
-date = "09/01/2020"
 # 1. Get current date
 today = datetime.now()
-
-today = pd.to_datetime(date)
 
 current_cal_year = today.year 
 
