83d2e5ae85bac7fea41cef8127068fad90be49e6
ngardner-3-1-8 <ngardner318@gmail.com>
2026-06-04 18:50:47 -0600

    Update daily_2_consolidate_predict_weekly_data.py

--- daily_2_consolidate_predict_weekly_data.py
+++ daily_2_consolidate_predict_weekly_data.py
@@ -1757,8 +1757,7 @@ def loop_through_simulations(date_str):
         df = schedule_df_with_odds_df
 
         print("CHECKING ODDS ISSUES")
-        print(df)
-        print(df['Total'].head(32))
+        print(df['Total Line'].head(32))
             
     
         df["Away Team Fair Odds"] = (
