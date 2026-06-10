9b05194035fc98a27873d04af9adda7ca22dc73f
ngardner-3-1-8 <ngardner318@gmail.com>
2026-06-09 22:36:43 -0600

    Update weekly_1_collect_last_weeks_pick_data.py

--- weekly_1_collect_last_weeks_pick_data.py
+++ weekly_1_collect_last_weeks_pick_data.py
@@ -221,8 +221,10 @@ def loop_through_historical_final_data(date_str):
     # If it's Jan 2026, current_year is 2025. This ensures we look at 2025 holidays.
     thanksgiving_season = get_thanksgiving(current_year)
     two_days_after_thanksgiving = thanksgiving_season + timedelta(days=2)
+    five_days_after_thanksgiving = thanksgiving_season + timedelta(days=5)
     
     christmas_season_cutoff = datetime(current_year, 12, 26)
+    christmas_season_cutoff_plus_one = christmas_season_cutoff + timedelta(days=1)
     
     # 3. Apply Adjustments
     # Use separate 'if' statements if you want them to be additive (+2 total)
@@ -232,20 +234,23 @@ def loop_through_historical_final_data(date_str):
             NUM_WEEKS_TO_KEEP += 1
             starting_week += 1
             last_played_week += 1
-            nfl_last_played_week += 1
+            if today < five_days_after_thanksgiving:
+                nfl_last_played_week += 1
     else:
         if today >= two_days_after_thanksgiving:
             print(f"Detected: Date is after {current_year} Thanksgiving. +1 to NUM_WEEKS_TO_KEEP.")
             NUM_WEEKS_TO_KEEP += 1
             starting_week += 1
             last_played_week += 1
-            nfl_last_played_week += 1
+            if today < five_days_after_thanksgiving:
+                nfl_last_played_week += 1
         if today >= christmas_season_cutoff:
             print(f"Detected: Date is after {current_year} Christmas. +1 to NUM_WEEKS_TO_KEEP.")
             NUM_WEEKS_TO_KEEP += 1
             starting_week += 1
             last_played_week += 1
-            nfl_last_played_week += 1
+            if today < christmas_season_cutoff_plus_one:
+                nfl_last_played_week += 1
     
     print(f"Final NUM_WEEKS_TO_KEEP: {NUM_WEEKS_TO_KEEP}")
     
