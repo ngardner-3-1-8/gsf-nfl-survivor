05dd4b87d18f2c75d5e000164f7715810cadda30
ngardner-3-1-8 <ngardner318@gmail.com>
2026-06-18 10:12:30 -0600

    Fix date condition and update commented dates

--- daily_2_consolidate_predict_weekly_data.py
+++ daily_2_consolidate_predict_weekly_data.py
@@ -97,7 +97,7 @@ def loop_through_simulations(date_str):
                 if today >= (boxing_day - timedelta(days=2)):
                     upcoming_week += 1
             elif target_year == 2023:
-                if today >= (boxing_day - timedelta(days=1)):
+                if today >= christmas_day:
                     upcoming_week += 1
             elif target_year in [2021, 2024,2025,2026]:
                 if today >= boxing_day:
@@ -5809,8 +5809,8 @@ if __name__ == "__main__":
 #        "11/29/2023", #Leading up to Week 14
 #        "12/06/2023", #Leading up to Week 15
 #        "12/13/2023", #Leading up to Week 16
-        "12/20/2023", #Leading up to Week 17
-#        "12/25/2023",  #Leading up to Week 18
+#        "12/20/2023", #Leading up to Week 17
+        "12/25/2023",  #Leading up to Week 18
 #        "12/27/2023", #Leading up to Week 19
 #        "01/03/2024", #Leading up to Week 20
         
