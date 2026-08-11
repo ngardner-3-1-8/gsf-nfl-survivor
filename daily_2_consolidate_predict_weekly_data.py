efe4b97be1b8d78dc027d40f440b7ad996b67019
ngardner-3-1-8 <ngardner318@gmail.com>
2026-08-11 13:40:12 -0600

    Update daily_2_consolidate_predict_weekly_data.py

--- daily_2_consolidate_predict_weekly_data.py
+++ daily_2_consolidate_predict_weekly_data.py
@@ -6188,10 +6188,10 @@ if __name__ == "__main__":
 #        "09/04/2024", #Leading up to Week 1
 #        "09/11/2024", #Leading up to Week 2
         "09/18/2024", #Leading up to Week 3
-#        "09/25/2024", #Leading up to Week 4
-#        "10/02/2024", #Leading up to Week 5
-#        "10/09/2024", #Leading up to Week 6
-#        "10/16/2024", #Leading up to Week 7
+        "09/25/2024", #Leading up to Week 4
+        "10/02/2024", #Leading up to Week 5
+        "10/09/2024", #Leading up to Week 6
+        "10/16/2024", #Leading up to Week 7
 #        "10/23/2024", #Leading up to Week 8
 #        "10/30/2024", #Leading up to Week 9
 #        "11/06/2024", #Leading up to Week 10
