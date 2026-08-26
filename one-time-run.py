e38c77b3ddba754045647dbfb8e95389f920ad26
ngardner-3-1-8 <ngardner318@gmail.com>
2026-08-25 18:11:44 -0600

    Save modified DataFrame to CSV
    Add a line to save the modified DataFrame back to the CSV file.

--- one-time-run.py
+++ one-time-run.py
@@ -4,3 +4,5 @@ import os
 
 df = pd.read_csv(f"nfl-power-ratings/final_sim_results_with_variance_week_1_2026.csv")
 df['Day of Week'] = pd.to_datetime(df['Date']).dt.day_name()
+
+df.to_csv(f"nfl-power-ratings/final_sim_results_with_variance_week_1_2026.csv", index=False)
