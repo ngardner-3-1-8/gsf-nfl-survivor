8a1785760093bdb917e564494c0d0b2645d809a9
ngardner-3-1-8 <ngardner318@gmail.com>
2026-09-15 14:34:40 -0600

    Refactor calendar date parsing for better handling

--- weekly_1_collect_last_weeks_pick_data.py
+++ weekly_1_collect_last_weeks_pick_data.py
@@ -477,8 +477,14 @@ def loop_through_historical_final_data(date_str):
     # Convert to string and clean up data types
     df_api_schedule['Week'] = df_api_schedule['Week'].astype(int)
 
-    df_api_schedule['Calendar Date'] = pd.to_datetime(df_api_schedule['Calendar Date'], format='%m/%d/%y').dt.strftime('%Y-%m-%d')
-    df_api_schedule['Calendar Date'] = df_api_schedule['Calendar Date'].dt.strftime('%Y-%m-%d')
+
+    def parse_calendar_date(s):
+        try:
+            return pd.to_datetime(s, format='%m/%d/%y')
+        except ValueError:
+            return pd.to_datetime(s, format='%Y-%m-%d')
+    
+    df_api_schedule['Calendar Date'] = df_api_schedule['Calendar Date'].apply(parse_calendar_date).dt.strftime('%Y-%m-%d')
     
     historical_home_df = pd.read_csv('contest-historical-data/Historical Home and Away data.csv')
     
