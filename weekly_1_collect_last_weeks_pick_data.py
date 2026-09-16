8b0212323b51366850e3503ad15000963900bbd4
ngardner-3-1-8 <ngardner318@gmail.com>
2026-09-16 11:44:19 -0600

    Fix team abbreviation for Los Angeles Rams

--- weekly_1_collect_last_weeks_pick_data.py
+++ weekly_1_collect_last_weeks_pick_data.py
@@ -1702,7 +1702,7 @@ def loop_through_historical_final_data(date_str):
         "CAR": "CAR", "CHI": "CHI", "CIN": "CIN", "CLE": "CLE",
         "DAL": "DAL", "DEN": "DEN", "DET": "DET", "GB": "GB",
         "HOU": "HOU", "IND": "IND", "JAX": "JAC", "KC": "KC",
-        "LAC": "LAC", "LAR": "LAR", "LV": "LV", "MIA": "MIA",
+        "LAC": "LAC", "LA": "LAR", "LV": "LV", "MIA": "MIA",
         "MIN": "MIN", "NE": "NE", "NO": "NO", "NYG": "NYG",
         "NYJ": "NYJ", "PHI": "PHI", "PIT": "PIT", "SEA": "SEA",
         "SF": "SF", "TB": "TB", "TEN": "TEN", "WAS": "WSH"
