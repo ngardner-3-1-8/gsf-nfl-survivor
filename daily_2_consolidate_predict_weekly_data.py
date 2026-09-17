3a39d6e0732a841f7c1e6f92cf7dd1f6bac0734a
ngardner-3-1-8 <ngardner318@gmail.com>
2026-09-16 23:47:58 -0600

    Comment out future dates in weekly data
    Comment out dates leading up to Week 5 and beyond.

--- daily_2_consolidate_predict_weekly_data.py
+++ daily_2_consolidate_predict_weekly_data.py
@@ -6725,7 +6725,7 @@ if __name__ == "__main__":
         "09/16/2020", #Leading up to Week 2
         "09/23/2020", #Leading up to Week 3
         "09/30/2020", #Leading up to Week 4
-        "10/07/2020", #Leading up to Week 5
+#        "10/07/2020", #Leading up to Week 5
 #        "10/14/2020", #Leading up to Week 6
 #        "10/21/2020", #Leading up to Week 7
 #        "10/28/2020", #Leading up to Week 8
