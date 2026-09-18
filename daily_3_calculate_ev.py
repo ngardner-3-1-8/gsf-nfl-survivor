e0ca2aa7da39c9bcbb31ff05a02cd946253197af
ngardner-3-1-8 <ngardner318@gmail.com>
2026-09-18 09:13:24 -0600

    Comment out week starting dates
    Commented out leading up to week starting dates.

--- daily_3_calculate_ev.py
+++ daily_3_calculate_ev.py
@@ -321,6 +321,8 @@ def loop_through_ev(date_str):
 if __name__ == "__main__":
     formatted_date = datetime.now().strftime("%m/%d/%Y")
     week_starting_dates = [
+#        "09/08/2026", #Leading up to Week 1
+        
 #        "09/03/2025", #Leading up to Week 1
 #        "09/10/2025", #Leading up to Week 2
 #        "09/17/2025", #Leading up to Week 3
