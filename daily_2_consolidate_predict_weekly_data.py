84a0095621c23b6c604d3fc9846a4efd579b0412
ngardner-3-1-8 <ngardner318@gmail.com>
2026-09-18 23:57:40 -0600

    Comment out week starting dates in main section
    Comment out the leading dates for week predictions.

--- daily_2_consolidate_predict_weekly_data.py
+++ daily_2_consolidate_predict_weekly_data.py
@@ -6616,7 +6616,7 @@ def loop_through_simulations(date_str):
 if __name__ == "__main__":
     formatted_date = datetime.now().strftime("%m/%d/%Y")
     week_starting_dates = [
-        "09/08/2026", #Leading up to Week 1
+#        "09/08/2026", #Leading up to Week 1
         
 #        "09/03/2025", #Leading up to Week 1
 #        "09/10/2025", #Leading up to Week 2
