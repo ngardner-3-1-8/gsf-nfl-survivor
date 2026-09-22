c387cb051039ae744c15fafb8a83325d371f145a
ngardner-3-1-8 <ngardner318@gmail.com>
2026-09-21 20:18:31 -0600

    Update daily_3_calculate_ev.py

--- daily_3_calculate_ev.py
+++ daily_3_calculate_ev.py
@@ -323,6 +323,7 @@ if __name__ == "__main__":
     formatted_date = datetime.now().strftime("%m/%d/%Y")
     week_starting_dates = [
 #        "09/08/2026", #Leading up to Week 1
+        "09/15/2026",
         
 #        "09/03/2025", #Leading up to Week 1
 #        "09/10/2025", #Leading up to Week 2
